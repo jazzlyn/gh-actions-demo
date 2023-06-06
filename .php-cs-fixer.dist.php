b0effbd807fbe967fcee35e1c22b667c30c13aa4
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2023-06-06 21:58:03 +0200

    use another php-cs-fixer config...

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,16 +1,7 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->exclude('somedir')
-    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
-    ->in(__DIR__)
-;
-
 $config = new PhpCsFixer\Config();
-return $config->setRules([
-        '@PSR12' => true,
-        'strict_param' => true,
-        'array_syntax' => ['syntax' => 'short'],
-    ])
-    ->setFinder($finder)
+return $config
+    ->setIndent("\t")
+    ->setLineEnding("\r\n")
 ;
