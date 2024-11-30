c94b03a377d2c132eb28045e51ca92a7e09550ef
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2024-11-30 20:59:48 +0100

    test terraform

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------------------
+# CONFIGURE TERRAFORM
+# --------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">= 1.5.0, <= 1.9.8"
+  required_providers {
+    # https://registry.terraform.io/providers/integrations/github/latest/docs
+    github = {
+      source  = "integrations/github"
+      version = "6.3.1"
+    }
+  }
+}
