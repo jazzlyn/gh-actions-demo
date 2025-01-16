6411abe7b312b437d7088061ff6f7add9dc5893f
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2025-01-16 20:31:16 +0100

    test renovate regex

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,8 @@ terraform {
     }
   }
 }
+
+locals {
+  # renovate: depName=ghcr.io/strg-at/containers/approve-merge-pr datasource=docker versioning=loose
+  image = "ghcr.io/strg-at/containers/approve-merge-pr:rolling@sha256:d77966db7cc8673f1a02a213b7ca68dd66d0531b94ddf28e998d07fe171398af"
+}
