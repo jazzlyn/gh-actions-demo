b1bcc03d5478e98de92eabb42cba4d93a4844049
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2025-05-28 21:33:09 +0200

    test files

---
+++ app-one/configs/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script is used to set up the environment for testing the application.
+# It sets the environment variable to indicate that the application is in test mode.
+export APP_ENV=test
